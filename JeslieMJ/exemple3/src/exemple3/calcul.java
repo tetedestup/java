3be8de098e8c729333d5c62d63f5a15d2cb0ea9f
Jeslie <jeslie@live.fr>
2018-11-29 16:38:23 +0100

    Mon exercice

---
+++ JeslieMJ/exemple3/src/exemple3/calcul.java
@@ -0,0 +1,27 @@
+package exemple3;
+
+public class calcul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 2;
+		int b = 4;
+		double c = 10;
+		double d = 20;
+		
+		
+		System.out.println("Le résultat est = " + (int) (a/b));
+		System.out.println("Le résultat est = " + (int) (a%b));
+		
+		System.out.println("Le résultat est = " +  (c/d));
+	    System.out.println("L'autre résultat est = " + (c%d));
+		
+		
+		
+		
+		
+		
+	}
+
+}
