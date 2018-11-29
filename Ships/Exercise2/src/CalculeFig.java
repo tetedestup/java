f4c12675375440c85c90dc05f4dcceecf84e15d1
Ships <macbookpro@MacBook-Pro-de-Mac.local>
2018-11-29 17:16:00 +0100

    Ex of 29/11/2018

---
+++ Ships/Exercise2/src/CalculeFig.java
@@ -0,0 +1,16 @@
+
+public class CalculeFig {
+
+	public static void main(String[] args) {
+		int a = 5, b = 2;
+		double c = 4, d = 3;
+		
+		System.out.println("iresultat " + (a / b));
+		System.out.println("imod " + (a % b));
+		System.out.println("dres " + (c / d));
+		System.out.println("dmodule " + (c % d)) ;
+		
+	}
+		
+}
+
