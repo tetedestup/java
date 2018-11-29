08fb623712850cf97f3c8c89e797a74deb62a2c6
Villa75 <45394741+Villa75@users.noreply.github.com>
2018-11-29 16:37:47 +0100

    mes classes calcul & comparateur & declaration

---
+++ villaGit/Declaration.java
@@ -0,0 +1,49 @@
+package exemple3;
+
+public class Declaration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		/*
+		String phrase ;
+		phrase = "Bonjour";
+		System.out.println(phrase);
+		
+		String str = "les" ;
+		System.out.println(str);
+		
+		String string = new String();	
+		string = "Simplons";
+		System.out.println(string);
+		
+		String chaine  = new String("JAVA");
+		System.out.println(chaine);
+		
+		
+		
+		System.out.println(); 
+		
+		System.out.println(phrase+" "+str+" "+string+" "+chaine); */
+		
+		
+		
+		
+		double   nbre1 = 10 , nbre2 = 3;
+		
+		int  resultat =   (int) nbre1 / ( int)  nbre2     ;  
+		System.out.println(" ca donne " + resultat);  
+	
+		
+		  
+		
+		 
+		
+
+		
+		
+		
+		
+}
+}
