dbcc8f51b1ce6155f08992bd21657f0bf9a36fed
LeilaBousbih <leila20493@gmail.com>
2018-11-29 17:06:22 +0100

    exercice 3

---
+++ LeilaBousbih/exple3/src/exple3/Declaration.java
@@ -0,0 +1,29 @@
+package exple3;
+
+public class Declaration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String phrase;
+		phrase = "voici mon exple";
+		
+		String str = new String();
+		str = "Une autre chaine de characteres";
+		
+		String string = "une autre chaine";
+		
+		String chaine = new String("et une de plus!");
+		
+	System.out.println(phrase+","+str+""+","+string+","+chaine);
+	
+	double nbre1 = 10, nbre2 = 3;
+	int resultat = (int)(nbre1 / nbre2);
+	
+	System.out.println("le resultat est = "  + (int) (nbre1) / (int) (nbre2));
+	//System.out.print("resultat =" nbre1 / nbre2);
+
+
+
+	}
+
+}
