32ba21c5c51a9bd0101f7f1c94d2c9c47434ce35
Jeslie <jeslie@live.fr>
2018-12-04 16:52:53 +0100

    Voici tous mes exos

---
+++ JeslieMJ/exemple5/src/exemple5/alphabet.java
@@ -0,0 +1,39 @@
+package exemple5;
+import java.util.Scanner;
+public class alphabet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		char ch = 'a';
+		
+		while(ch <='z')
+		{
+			System.out.println(ch);
+			ch ++;
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ JeslieMJ/exemple5/src/exemple5/nombres.java
@@ -0,0 +1,27 @@
+package exemple5;
+import java.util.Scanner;
+public class nombres {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Veuillez entrer votre nombre entre 1 et 10");
+		Scanner clavier= new Scanner(System.in);
+		
+		int nbr= clavier.nextInt();
+		
+		while (nbr <=10 ) {
+			System.out.println(nbr);
+			nbr++;
+		}
+		
+ 
+		
+				
+					
+					
+				
+	}
+
+}
